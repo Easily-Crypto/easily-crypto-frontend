5f820f328a15b2311f79ee05dfc2388485fbdd24
iasmim <iasmimstuff@gmail.com>
2022-09-13 20:52:02 -0300

    feat: homepage elements

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,36 @@
-import HeaderSection from "../../components/Header";
-import { Page } from "../../components/Page";
+import HeaderSection from '../../components/Header';
+import { Page } from '../../components/Page';
+import Bitcoin from '../../assets/bitcoin.svg';
+import Bars from '../../assets/bars.svg';
+import Graphic from '../../assets/graphic.svg';
+import { DescriptionSection, Content, ImageSection } from './styled';
+import { useUser } from '../../Providers/user';
+import Card from '../../components/Card';
 
 const Home = () => {
+  // const {userInfo} = useUser()
+  // console.log(userInfo)
   return (
     <Page>
-      <HeaderSection></HeaderSection>
+      <HeaderSection />
+      <Content>
+        <DescriptionSection>
+          <p>Olá, fulano!</p>
+          <Card
+            img={Bars}
+            title='Gerencie suas carteiras de criptomoeda'
+            description='Aqui na Easily Crypto, você pode criar e gerenciar carteiras de criptomoeda de maneira fácil e acessível'
+          ></Card>
+          <Card
+            img={Graphic}
+            title='Faça seu investimento valer a pena'
+            description='Somos comprometidos com nossos clientes, e colocamos a transparência em primeiro lugar'
+          ></Card>
+        </DescriptionSection>
+        <ImageSection>
+          <img src={Bitcoin} alt='two people between a bitcoin' />
+        </ImageSection>
+      </Content>
     </Page>
   );
 };
---
+++ src/pages/Home/styled.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  width: 90%;
+  height: 80vh;
+  padding-top: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const DescriptionSection = styled.section`
+  width: 50%;
+  font-size: 22px;
+  color: #f3f3f3;
+`;
+
+export const ImageSection = styled.section`
+  img {
+    width: 500px;
+  }
+`;
+
+
